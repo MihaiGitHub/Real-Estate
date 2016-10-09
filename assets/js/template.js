c15cfa93b0012103cf4da86b40eff99287edf604
MihaiSmarandache <mihai.sanfran@gmail.com>
2016-10-09 22:39:36 +0800

    Updated navigation menu and map

--- assets/js/template.js
+++ assets/js/template.js
@@ -42,9 +42,9 @@ function center(number) {
 function initialize() {
 	var bodyClass = jQuery('body').hasClass('property-listing-page map'),
 		assetPath = (bodyClass == true ?  "../" : ''),
-		myLatLng   = new google.maps.LatLng(47.609519, 12.852459);
+		myLatLng   = new google.maps.LatLng(52.2297, 21.0122);
 	var mapOptions = {
-		zoom:               5,
+		zoom:               6,
 		center:             myLatLng,
 		// This is where you would paste any style found on Snazzy Maps.
 		// styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}],
